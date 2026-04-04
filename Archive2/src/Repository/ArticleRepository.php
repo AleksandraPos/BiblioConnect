86a1896dc719a1e94cda399f006f6a7daabab0e9
Aleksandra Poselskaia <134145571+AleksandraPos@users.noreply.github.com>
2026-04-04 13:49:28 +0200

    feat: structure principale, gestion des rôles et redirection après login

---
+++ Archive2/src/Repository/ArticleRepository.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Article;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Article>
+ */
+class ArticleRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Article::class);
+    }
+
+
+        public function findRecentPublished(int $limit = 10): array
+       {
+           return $this->createQueryBuilder('a')
+               ->orderBy('a.id', 'DESC')
+               ->setMaxResults($limit)
+               ->getQuery()
+               ->getResult()
+           ;
+       }
+        public function findBySearch($value): array
+       {
+           return $this->createQueryBuilder('a')               
+               ->andWhere('a.title LIKE :val')
+               ->setParameter('val', '%'.$value.'%')
+               ->getQuery()
+               ->getResult()
+           ;
+       }
+
+    //    /**
+    //     * @return Article[] Returns an array of Article objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('a.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Article
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
